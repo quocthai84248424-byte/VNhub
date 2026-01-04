9d96a9e99bfc14b469f0f20c3bcb4af9689b5c1b
SeaMinh <seaminh.smh@gmail.com>
2026-01-04 08:46:04 +0700

    Update 2.lua

--- 2.lua
+++ 2.lua
@@ -1,1 +1,19 @@
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
+local Window = WindUI:CreateWindow({
+    Title = "Huyền Vũ Tông - Forsaken",
+    Icon = "rbxassetid://84155435407343", -- lucide icon
+    Author = "BY ⟦ 春秋禪 ⟧",
+    Folder = "MySuperHub",
+    
+    Size = UDim2.fromOffset(580, 460),
+    MinSize = Vector2.new(560, 350),
+    MaxSize = Vector2.new(850, 560),
+    Transparent = true,
+    Theme = "Dark",
+    Resizable = true,
+    SideBarWidth = 200,
+    BackgroundImageTransparency = 0.42,
+    HideSearchBar = true,
+    ScrollBarEnabled = false,
+})
